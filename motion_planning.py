7133dd090ad774797024e4e799c7b27b87cb8321
Chu-Chu Igbokwe <raider64x@gmail.com>
2018-06-07 00:12:10 -0700

    5/10 done

--- motion_planning.py
+++ motion_planning.py
@@ -124,14 +124,21 @@ class MotionPlanning(Drone):
         # TODO: read lat0, lon0 from colliders into floating point values
         file = open('colliders.csv', 'r')
         reader = csv.reader(file)
-        header = reader.next()
+        header = reader.__next__()
+
         lat0, lon0 = [float(header[i].split()[1]) for i, j in enumerate(header)]
         # TODO: set home position to (lon0, lat0, 0)
-        self.global_home = (lon0, lat0, 0)
+        global_home = (lon0, lat0, 0)
+        self.set_home_position(lon0, lat0, 0)
+        # print(self.global_home, self.global_position)
+
         # TODO: retrieve current global position
-        self.global_position = self
+        global_position = (self._longitude, self._latitude, self._altitude)
+        # print(global_position)
         # TODO: convert to current local position using global_to_local()
-        self.local_position = global_to_local(self.global_position, self.global_home)
+        local_position = global_to_local(global_position, global_home)
+        local_north, local_east, local_down = local_position
+        print(local_position)
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
         # Read in obstacle map
@@ -141,9 +148,9 @@ class MotionPlanning(Drone):
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
-        grid_start = (-north_offset, -east_offset)
+        # grid_start = (-north_offset + int(local_north), -east_offset + int(local_east))
         # TODO: convert start position to current position rather than map center
-        
+        grid_start = (-north_offset + int(local_north), -east_offset + int(local_east))
         # Set goal as some arbitrary position on the grid
         grid_goal = (-north_offset + 10, -east_offset + 10)
         # TODO: adapt to set goal as latitude / longitude position and convert
